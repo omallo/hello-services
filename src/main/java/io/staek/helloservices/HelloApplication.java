6ace037a64ee9a3110d6d2525e1e1b2163246509
Ovidio Jose Mallo <ovidio.mallo@netcetera.com>
2022-06-17 12:40:56 +0200

    greet through http

--- src/main/java/io/staek/helloservices/HelloApplication.java
+++ src/main/java/io/staek/helloservices/HelloApplication.java
@@ -1,13 +1,23 @@
 package io.staek.helloservices;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HelloApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloApplication.class, args);
+	}
+
+	@GetMapping(path = "/", produces = MediaType.TEXT_PLAIN_VALUE)
+	public String greet(@Value("${greeting}") String greeting) {
+		return greeting;
 	}
 
 }
