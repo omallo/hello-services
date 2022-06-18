851b96b583e744c4a7c534a4cd3f28cebd3dc478
Ovidio Jose Mallo <ovidio.mallo@netcetera.com>
2022-06-18 12:21:41 +0200

    cleanup

--- src/main/java/io/staek/helloservices/HelloApplication.java
+++ src/main/java/io/staek/helloservices/HelloApplication.java
@@ -1,5 +1,7 @@
 package io.staek.helloservices;
 
+import java.net.InetAddress;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,17 +9,24 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.SneakyThrows;
+
 @SpringBootApplication
 @RestController
 public class HelloApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HelloApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(HelloApplication.class, args);
+  }
 
-	@GetMapping(path = "/", produces = MediaType.TEXT_PLAIN_VALUE)
-	public String greet(@Value("${greeting}") String greeting) {
-		return greeting;
-	}
+  @SneakyThrows
+  @GetMapping(path = "/", produces = MediaType.TEXT_PLAIN_VALUE)
+  public String greet(@Value("${hello.greeting:}") String greeting, @Value("${hello.secret:}") String secret) {
+    StringBuilder sb = new StringBuilder();
+    sb.append("hostname : ").append(InetAddress.getLocalHost().getHostName()).append('\n');
+    sb.append("greeting : ").append(greeting).append('\n');
+    sb.append("secret   : ").append(secret).append('\n');
+    return sb.toString();
+  }
 
 }
